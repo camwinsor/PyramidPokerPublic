07441fef4be5148b44f610778d00655a2ebf2352
theloniuspunk <winsor.cam@gmail.com>
2017-05-18 12:38:01 -0700

    sync from private repo

--- privateToPublicCopy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-rsync -rv --exclude=.git --exclude=.gitignore --delete /Users/CWins/AndroidStudioProjects/AndroidFlashPoker/ /Users/CWins/source/Pyramid/PyramidPokerPublic/
-
-
